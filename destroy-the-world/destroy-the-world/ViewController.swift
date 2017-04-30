18ec933212ef6c4e8f1b830e1a53ab299e340e2b
Karen Hughes <Karen@curlypaws.com>
2017-04-30 14:24:49 +0100

    Initial commit

---
+++ destroy-the-world/destroy-the-world/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  destroy-the-world
+//
+//  Created by Karen Hughes on 30/04/2017.
+//  Copyright © 2017 Karen Hughes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Hello, World!")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
